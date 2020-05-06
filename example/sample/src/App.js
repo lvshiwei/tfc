67393f87c64b2cb639a1fd2d26ad2b875ef0959f
lvshiwei <shiwei.lv@luckincoffee.com>
2020-05-06 16:25:24 +0800

    add example folder

---
+++ example/sample/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Submit</Button>
+    </div>
+  );
+}
+
+export default App;
