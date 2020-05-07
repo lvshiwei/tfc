b9eae30368e70aa11ff126182a0aa6f3570b8d95
lvshiwei <shiwei.lv@luckincoffee.com>
2020-05-07 17:04:41 +0800

    modify readme.md

--- jsonMock.js
+++ jsonMock.js
@@ -1,7 +1,5 @@
 export default {
-  //编号
-  expandProjectNo: 'TM20391929',
-  // 门店地址
+  // 地址
   // @max 30
   shopLocation: '厦门市湖里区',
   // 经度
@@ -11,23 +9,14 @@ export default {
   // @min 0, XXX, yyy, true, 3
   // @max 19999,不能太大
   preLatitude: 25.03949,
-  // 预开业时间
+  // 开始时间
   // @date
-  // @required 请填写预开业日期
+  // @required 请填写预开始时间
   // @max 2099/12/31,不能太晚
   // @nullable false, 说点啥
   preOpeningTime: 1586854463211,
-  // 支持堂食
-  // @required 请选择是否支持堂食
-  isSupportCanteen: 1,
-  // 座位数
-  // @number
-  // @required 请填写座位数
-  // @min 1, 请填写正确的座位数
-  //
-  seatsNumber: 20,
-  // 生效的
+  // 立刻生效
   // @bool
-  // @required 请选择是否生效
+  // @required 请选择是否立刻生效
   active: true,
 };
